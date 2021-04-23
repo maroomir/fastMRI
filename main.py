178ce438257c9b2d434ccea402f42a6f6b60cfdb
maroomir <maroomir@gmail.com>
2021-04-24 08:54:09 +0900

    Change the main function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import fastmri_examples.unet.unet_knee_sc_leaderboard_20201111
+from argparse import ArgumentParser
 
-
-def process_main():
-    fastmri_examples.unet.unet_knee_sc_leaderboard_20201111.run_cli()
-
-
-if __name__ == "__main__":
-    process_main()
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.set_defaults(
+        challenge="singlecoil",
+        data_path="D:\Git\FastMRI\Data",
+        mask_type="random",
+        sample_rate=0.1)
+    args = parser.parse_args()
+    fastmri_examples.unet.unet_knee_sc_leaderboard_20201111.cli_main(args=args)
